1dbcc90a6edddd26d14e2ad8b4b515b1eef29cd0
Sbaby <corwin808@gmail.com>
2021-01-24 23:19:35 +0800

    first commit!

---
+++ src/main/java/com/mavis/chattingroom/MavisApplication.java
@@ -0,0 +1,13 @@
+package com.mavis.chattingroom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MavisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mavis/chattingroom/MavisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mavis.chattingroom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

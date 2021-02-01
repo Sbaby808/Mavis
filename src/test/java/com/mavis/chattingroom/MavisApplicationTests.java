852f06ce4f330d82fe686dfa6e9d7cd26c5d5543
Sbaby <corwin808@gmail.com>
2021-02-01 17:16:59 +0800

    添加druid监控配置

--- src/test/java/com/mavis/chattingroom/MavisApplicationTests.java
+++ src/test/java/com/mavis/chattingroom/MavisApplicationTests.java
@@ -3,11 +3,16 @@ package com.mavis.chattingroom;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.math.BigDecimal;
+
 @SpringBootTest
 class MavisApplicationTests {
 
     @Test
     void contextLoads() {
+        BigDecimal b = new BigDecimal("123");
+        String r = b.setScale(10).toString();
+        System.out.println(r);
     }
 
 }

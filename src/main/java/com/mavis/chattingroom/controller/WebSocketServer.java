bd2b8b122401e37128c16f866d001e4a660e0c59
xinjingjie <1090282309@qq.com>
2021-02-20 17:20:32 +0800

    添加websocket的server

---
+++ src/main/java/com/mavis/chattingroom/controller/WebSocketServer.java
@@ -0,0 +1,105 @@
+package com.mavis.chattingroom.controller;
+
+import com.mavis.chattingroom.util.JsonUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import javax.websocket.OnClose;
+import javax.websocket.OnMessage;
+import javax.websocket.OnOpen;
+import javax.websocket.Session;
+import javax.websocket.server.PathParam;
+import javax.websocket.server.ServerEndpoint;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @Author： xinjingjie
+ * @Date：2021/2/20 16:06
+ **/
+@ServerEndpoint("/chat/{userId}")
+@Component
+public class WebSocketServer {
+    static Logger logger = LoggerFactory.getLogger(WebSocketServer.class);
+    private static final AtomicInteger onlineCount = new AtomicInteger();
+    private static final ConcurrentHashMap<String, WebSocketServer> webSocketMap = new ConcurrentHashMap<>();
+    private Session session;
+    private String userId;
+
+    /**
+     * 建立连接
+     * @param session
+     * @param userId
+     */
+    @OnOpen
+    public void open(Session session, @PathParam("userId") String userId) {
+        this.session = session;
+        this.userId = userId;
+        if (webSocketMap.containsKey(userId)) {
+            webSocketMap.remove(userId);
+            webSocketMap.put(userId, this);
+            logger.info("已有userId:" + userId + "：重新登录");
+
+        } else {
+            webSocketMap.put(userId, this);
+            onlineCount.incrementAndGet();
+            logger.error("请求的userId:" + userId + "：新加入用户");
+        }
+    }
+
+    /**
+     * 接收消息
+     * @param message
+     * @param session
+     */
+    @OnMessage
+    public void getMessage(String message, Session session) {
+        if ("".equals(message)) {
+            try {
+                //解析发送的报文
+                HashMap jsonObject = JsonUtils.parse(message, HashMap.class);
+                //追加发送人(防止串改)
+                jsonObject.put("fromUserId", this.userId);
+                String toUserId = (String) jsonObject.get("toUserId");
+                //传送给对应toUserId用户的websocket
+                if (!"".equals(toUserId) && webSocketMap.containsKey(toUserId)) {
+                    webSocketMap.get(toUserId).sendMessage(JsonUtils.toJsonString(jsonObject));
+                } else {
+                    logger.error("请求的userId:" + toUserId + "不在该服务器上");
+                    //否则不在这个服务器上，发送到mysql或者redis
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * 实现服务器主动推送
+     */
+    public void sendMessage(String message) throws IOException {
+        this.session.getBasicRemote().sendText(message);
+    }
+
+    /**
+     * 连接关闭调用的方法
+     */
+    @OnClose
+    public void onClose() {
+        if (webSocketMap.containsKey(userId)) {
+            webSocketMap.remove(userId);
+            //从set中删除
+            onlineCount.decrementAndGet();
+        }
+        logger.info("用户退出:" + userId + ",当前在线人数为:" + getOnlineCount());
+    }
+
+    private int getOnlineCount() {
+        return onlineCount.get();
+    }
+
+
+}
---
+++ src/main/java/com/mavis/chattingroom/util/JsonUtils.java
@@ -0,0 +1,72 @@
+package com.mavis.chattingroom.util;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.lang.NonNull;
+
+/**
+ * @Author： xinjingjie
+ * @Date：2021/2/20 16:49
+ **/
+public class JsonUtils {
+    private static final ObjectMapper mapper = new ObjectMapper();
+    private static final Logger log= LoggerFactory.getLogger(JsonUtils.class);
+    static {
+        // 对于空的对象转json的时候不抛出错误
+        mapper.disable(SerializationFeature.FAIL_ON_EMPTY_BEANS);
+        // 允许属性名称没有引号
+        mapper.configure(JsonParser.Feature.ALLOW_UNQUOTED_FIELD_NAMES, true);
+        // 允许单引号
+        mapper.configure(JsonParser.Feature.ALLOW_SINGLE_QUOTES, true);
+        // 设置输入时忽略在json字符串中存在但在java对象实际没有的属性
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+        // 设置输出时包含属性的风格
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    }
+
+
+    /**
+     * 序列化，将对象转化为json字符串
+     *
+     * @param data
+     * @return
+     */
+    public static String toJsonString(Object data) {
+        if (data == null) {
+            return null;
+        }
+
+        String json = null;
+        try {
+            json = mapper.writeValueAsString(data);
+        } catch (JsonProcessingException e) {
+            log.error("[{}] toJsonString error：{{}}", data.getClass().getSimpleName(), e);
+        }
+        return json;
+    }
+
+
+    /**
+     * 反序列化，将json字符串转化为对象
+     *
+     * @param json
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    public static <T> T parse(@NonNull String json, Class<T> clazz) {
+        T t = null;
+        try {
+            t = mapper.readValue(json, clazz);
+        } catch (Exception e) {
+            log.error(" parse json [{}] to class [{}] error：{{}}", json, clazz.getSimpleName(), e);
+        }
+        return t;
+    }
+}
